bcaaf837c47adf65a42a1d74a2eee2c7c65ca713
Jiobu <jisha@obukwelu.com>
2014-05-05 09:17:49 -0400

    Initial Commit

---
+++ Classwork/05052014/PixelNoise/PixelNoise/PNAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PNAAppDelegate.h
+//  PixelNoise
+//
+//  Created by Jisha Obukwelu on 5/5/14.
+//  Copyright (c) 2014 Jisha Obukwelu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PNAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Classwork/05052014/PixelNoise/PixelNoise/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PixelNoise
+//
+//  Created by Jisha Obukwelu on 5/5/14.
+//  Copyright (c) 2014 Jisha Obukwelu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PNAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PNAAppDelegate class]));
+    }
+}
