870b2681900bbc0e7348f85b7393ca4ca9fdb8f9
Jiobu <jisha@obukwelu.com>
2014-05-14 11:22:52 -0400

    CoreData
    saving and deleting in core data

---
+++ Classwork/05140214/MyFirstData/MyFirstData/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyFirstData
+//
+//  Created by Jisha Obukwelu on 5/14/14.
+//  Copyright (c) 2014 Jisha Obukwelu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MFDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MFDAppDelegate class]));
+    }
+}
