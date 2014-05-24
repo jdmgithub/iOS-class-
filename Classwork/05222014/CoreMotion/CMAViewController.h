553328dd8253e84d478adfaad16de65c4ed6f728
Jiobu <jisha@obukwelu.com>
2014-05-24 14:49:17 -0400

    Core Motion

---
+++ Classwork/05222014/CoreMotion/CMAViewController.h
@@ -0,0 +1,13 @@
+//
+//  CMAViewController.h
+//  CoreMotion
+//
+//  Created by Jisha Obukwelu on 5/22/14.
+//  Copyright (c) 2014 Jisha Obukwelu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CMAViewController : UIViewController
+
+@end
---
+++ Classwork/05222014/CoreMotion/CoreMotion/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CoreMotion
+//
+//  Created by Jisha Obukwelu on 5/22/14.
+//  Copyright (c) 2014 Jisha Obukwelu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CMAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CMAAppDelegate class]));
+    }
+}
