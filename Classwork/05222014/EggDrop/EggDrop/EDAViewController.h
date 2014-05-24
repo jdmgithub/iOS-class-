502c857847478796a96064ab34220310f0e8faca
Jiobu <jisha@obukwelu.com>
2014-05-24 14:48:31 -0400

    EggDrop
    create egg drop game
    - import photos
    - sound
    - touches
    - if statement to control sizing

---
+++ Classwork/05222014/EggDrop/EggDrop/EDAViewController.h
@@ -0,0 +1,16 @@
+//
+//  EDAViewController.h
+//  EggDrop
+//
+//  Created by Jisha Obukwelu on 5/22/14.
+//  Copyright (c) 2014 Jisha Obukwelu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EDAViewController : UIViewController
+
+@property(weak, nonatomic)IBOutlet UIImageView * spoon;
+@property(weak, nonatomic)IBOutlet UIImageView * egg;
+@property(nonatomic) IBOutlet UIButton * resetGame;
+@end
