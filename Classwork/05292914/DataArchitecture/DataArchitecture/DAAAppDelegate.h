79da9597daa3a72e3bd7f82892e2b8f9e2b7d9c3
Jiobu <jisha@obukwelu.com>
2014-05-29 09:48:46 -0400

    DataArchitecture
    working with dictionaries and arrays nested inside each other

---
+++ Classwork/05292914/DataArchitecture/DataArchitecture/DAAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DAAAppDelegate.h
+//  DataArchitecture
+//
+//  Created by Jisha Obukwelu on 5/29/14.
+//  Copyright (c) 2014 Jisha Obukwelu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DAAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
