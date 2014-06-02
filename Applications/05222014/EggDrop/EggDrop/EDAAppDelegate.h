71781e31b189573316bffb1ba6e57c49512d17ae
Jiobu <jisha@obukwelu.com>
2014-06-01 23:08:12 -0400

    Egg Drop
    update

---
+++ Applications/05222014/EggDrop/EggDrop/EDAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EDAAppDelegate.h
+//  EggDrop
+//
+//  Created by Jisha Obukwelu on 5/22/14.
+//  Copyright (c) 2014 Jisha Obukwelu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EDAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
