0b953d085b3121552301ba4a4060da3caf58f029
Jiobu <jisha@obukwelu.com>
2014-06-01 23:05:12 -0400

    Twitter Post
    update

---
+++ Applications/05202014/TwitterPosts/TwitterPosts/TPAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TPAAppDelegate.h
+//  TwitterPosts
+//
+//  Created by Jisha Obukwelu on 5/20/14.
+//  Copyright (c) 2014 Jisha Obukwelu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TPAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
