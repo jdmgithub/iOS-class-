20fa447c0a4466295ee654617c807108bcfc9f8d
Jiobu <jisha@obukwelu.com>
2014-06-01 22:46:38 -0400

    TweetLike
    update

---
+++ Applications/04232014/TweetLike/TweetLike/TLAAppDelegate.m
@@ -0,0 +1,34 @@
+//
+//  TLAAppDelegate.m
+//  TweetLike
+//
+//  Created by Jisha Obukwelu on 4/23/14.
+//  Copyright (c) 2014 Jisha Obukwelu. All rights reserved.
+//
+
+#import "TLAAppDelegate.h"
+#import "TLATableViewController.h"
+#import "TLANavController.h"
+
+@implementation TLAAppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    // Override point for customization after application launch.
+//    self.window.rootViewController = [[TLATableViewController alloc] initWithStyle:UITableViewStylePlain];
+    
+//    self.window.rootViewController = [[TLANavController alloc] initWithRootViewController:[[TLATableViewController alloc] initWithStyle:UITableViewStylePlain]];
+    
+    TLANavController * nc = [[TLANavController alloc] init];
+    
+    self.window.rootViewController = nc;
+    
+    [nc addTableViewController:[[TLATableViewController alloc] initWithStyle:UITableViewStylePlain]];
+
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+@end
