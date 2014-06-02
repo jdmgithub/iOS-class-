9a3f237fce5cf36ef36f762eeacfc9c00220fa77
Jiobu <jisha@obukwelu.com>
2014-06-01 22:56:08 -0400

    Digital Pond
    update

---
+++ Applications/05140214/DigitalPond/DPARipple.h
@@ -0,0 +1,15 @@
+//
+//  DPARipple.h
+//  DigitalPond
+//
+//  Created by Jisha Obukwelu on 5/14/14.
+//  Copyright (c) 2014 Jisha Obukwelu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DPARipple : UIView
+
+@property (nonatomic) float rippleCount;
+@property (nonatomic) float rippleLifeTime;
+@end
