6fc90588d62b2e69f1dd5e3d9ffd266a9829202b
Jiobu <jisha@obukwelu.com>
2014-06-01 23:10:11 -0400

    Data Architecture
    update

---
+++ Applications/05292914/DataArchitecture/DataArchitecture/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DataArchitecture
+//
+//  Created by Jisha Obukwelu on 5/29/14.
+//  Copyright (c) 2014 Jisha Obukwelu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DAAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DAAAppDelegate class]));
+    }
+}
