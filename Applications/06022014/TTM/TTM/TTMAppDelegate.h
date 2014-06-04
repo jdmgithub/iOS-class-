1e5b8a289d1361cd63ed25ac647c3c1b4683b720
Jiobu <jisha@obukwelu.com>
2014-06-03 23:48:13 -0400

    Parse Push Notification
    sending Push Notifications

---
+++ Applications/06022014/TTM/TTM/TTMAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  TTMAppDelegate.h
+//  TTM
+//
+//  Created by Jisha Obukwelu on 6/2/14.
+//  Copyright (c) 2014 Jisha Obukwelu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
+
+@interface TTMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
